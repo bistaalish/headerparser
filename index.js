558887839291f5c4e66d7bc9dfede96fae6fd3be
Alish Bista <bistace321@gmail.com>
2023-08-21 20:24:57 +0545

    added comment

--- index.js
+++ index.js
@@ -1,48 +1,59 @@
 // index.js
 // where your node app starts
 
-// init project
+// Load environment variables from .env file
 require('dotenv').config();
-var express = require('express');
-var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
-var cors = require('cors');
+// Import required modules
+const express = require('express');
+const app = express();
+
+// Enable Cross-Origin Resource Sharing (CORS)
+const cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
+// Serve static files from the "public" directory
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
+// Route for the root URL
 app.get('/', function (req, res) {
+  // Send the index.html file as a response
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
+// Define an API endpoint for a basic "hello" response
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-// Adding console logging middleware
-const logMiddleware = (req,res,next) => {
-  var string = req.method + " " + req.path + " - " + req.ip;
-  console.log(string)
-  next();
-}
-// Adding endpoint /api/whoami
-app.use("/api/whoami",(req,res)=>{
-   // Get the preferred language from the "Accept-Language" header
-   const preferredLanguage = req.headers['accept-language'];
+// Middleware for logging requests
+const logMiddleware = (req, res, next) => {
+  // Create a log string containing request method, path, and IP address
+  const logString = `${req.method} ${req.path} - ${req.ip}`;
+  console.log(logString);
+  next(); // Move to the next middleware or route handler
+};
 
-  // Get user ip
-  const ip = req.ip
-  // Get user agent
+// Define an API endpoint for /api/whoami
+app.use('/api/whoami', (req, res) => {
+  // Get the preferred language from the "Accept-Language" header
+  const preferredLanguage = req.headers['accept-language'];
+
+  // Get user IP address
+  const ip = req.ip;
+
+  // Get user agent (browser and OS information)
   const ua = req.get('User-Agent');
-  res.json({ipaddress:ip,language:preferredLanguage,software:ua});
-})
+
+  // Send a JSON response with IP address, preferred language, and user agent
+  res.json({ ipaddress: ip, language: preferredLanguage, software: ua });
+});
+
+// Attach the logging middleware to every request
 app.use(logMiddleware);
-// listen for requests :)
-var listener = app.listen(process.env.PORT || 3001, function () {
+
+// Start the server and listen for incoming requests
+const port = process.env.PORT || 3001;
+const listener = app.listen(port, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
